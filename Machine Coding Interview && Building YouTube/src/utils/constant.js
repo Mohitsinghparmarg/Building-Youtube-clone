6f13ab9f80cd69cba4aa3da7b269026438e30ad6
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-04-07 04:35:09 +0530

    Add files via upload

---
+++ Machine Coding Interview && Building YouTube/src/utils/constant.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = 'AIzaSyAHX5Lt0kVE36qHRBsjYvJIAoAQ6gRj9tc';
+
+export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + GOOGLE_API_KEY;
